69e2a12c3b0135325398f1e86705a1be7f25b5f6
Ally Netzel <netzelally@gmail.com>
2018-03-30 00:48:01 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   new_array = array.join(",")
   if array.length < 3
     array.join(" and ")
-  end
+
   elsif new_array.length > 4
     array.join(",") + ("and") + array[-1]
   end
