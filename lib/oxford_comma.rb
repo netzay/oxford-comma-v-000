de3d633e6673cb1400ed561aad378b9412b05ebb
Ally Netzel <netzelally@gmail.com>
2018-03-30 00:51:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
+  new_array = []
+  new_array = array.join(",")
+  if array.length < 3
+    array.join(" and ")
 
+  elsif new_array.length > 4
+    array[-1] = "and #{array[-1]}"
+    array.join(', ')
+  end
 end
